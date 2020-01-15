b58f110de7e220ba8d453d4ec94ffb1a786b1514
Roman Filippov <rf@romanfilippov.com>
2020-01-15 14:20:32 +0700

    feat: adds United Arab Emirates support

--- src/marketplaces/AE.ts
+++ src/marketplaces/AE.ts
@@ -7,4 +7,17 @@ export const AE = new AmazonMarketplace({
   name: 'United Arab Emirates',
   uri: 'https://www.amazon.ae',
   webServiceUri: 'https://mws.amazonservices.ae',
+  advertising: {
+    uri: 'https://advertising-api-eu.amazon.com',
+    bids: {
+      sponsoredBrands: {
+        min: 24,
+        max: 18400,
+      },
+      sponsoredProducts: {
+        min: 24,
+        max: 18400,
+      },
+    },
+  },
 })
