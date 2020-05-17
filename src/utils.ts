33113b74306f0738eb1bec2108429669dda39c64
Roman Filippov <rf@romanfilippov.com>
2020-05-17 15:41:40 +0700

    feat: adds assertMarketplaceHasAdvertising assertion fn

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-import { AmazonMarketplace } from './amazon-marketplace'
+import { AmazonMarketplace, AmazonMarketplaceAdvertising } from './amazon-marketplace'
 import { amazonMarketplacesList } from './marketplaces'
 import ensure from '@lavadrop/ensure'
 
@@ -21,4 +21,17 @@ export function findAmazonMarketplaceOrFail<K extends Key>(key: K, value: Amazon
     findAmazonMarketplace(key, value),
     `Cannot find Amazon Marketplace by key "${key}" for value "${value}".`,
   )
+}
+
+/**
+ * Assert function that guards the marketplace and asserts that it has the advertising capability.
+ *
+ * See [Assertion Functions](https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-7.html#assertion-functions) docs.
+ */
+export function assertMarketplaceHasAdvertising(
+  marketplace: AmazonMarketplace,
+): asserts marketplace is AmazonMarketplace & { advertising: AmazonMarketplaceAdvertising } {
+  if (!marketplace.advertising) {
+    throw new Error(`Marketplace ${marketplace.id} does not have advertising.`)
+  }
 }
