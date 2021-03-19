df8ea888bce134188c03c51912897b8d6cb298b8
Nazar Vovk <nazarvovk.work@gmail.com>
2021-03-19 09:56:49 +0200

    feat: export selling partner regions

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export * from './amazon-marketplace'
 export * from './marketplaces'
 export * from './utils'
+export * from './amazon-selling-partner-api-region'
+export * from './selling-partner-api-regions'
