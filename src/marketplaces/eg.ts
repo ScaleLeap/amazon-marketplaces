9b0c36cd196291ca95a7263eb1900ac11c0db15e
Toan Nguyen <nguyentoandev@gmail.com>
2022-08-02 15:46:43 +0700

    feat: add Egypt to advertising
    - add Egypt to advertising
    - update `mx` Sponsored Brands bid
    - update `pl` Sponsored Products bid

--- src/marketplaces/eg.ts
+++ src/marketplaces/eg.ts
@@ -1,8 +1,11 @@
 import {
   AmazonMarketplace,
+  AmazonMarketplaceAdvertisingCountryCode,
   AmazonMarketplaceAdvertisingCurrency,
+  AmazonMarketplaceAdvertisingTimeZone,
   AmazonMarketplaceCountryCode,
 } from '../amazon-marketplace'
+import { marketplaceAdvertisingRegions } from '../marketplace-advertising-regions'
 import { sellingPartnerRegions } from '../selling-partner-api-regions'
 
 export const EG = new AmazonMarketplace({
@@ -13,6 +16,21 @@ export const EG = new AmazonMarketplace({
   uri: 'https://www.amazon.eg',
   vendorCentralUri: 'https://vendorcentral.amazon.me',
   webServiceUri: 'https://mws-eu.amazonservices.com',
+  advertising: {
+    countryCode: AmazonMarketplaceAdvertisingCountryCode.EG,
+    region: marketplaceAdvertisingRegions.EU,
+    bids: {
+      sponsoredBrands: {
+        min: 70,
+        max: 40000,
+      },
+      sponsoredProducts: {
+        min: 15,
+        max: 550,
+      },
+    },
+    timeZone: AmazonMarketplaceAdvertisingTimeZone.AFRICA_CAIRO,
+  },
   sellingPartner: {
     region: sellingPartnerRegions.EU,
   },
