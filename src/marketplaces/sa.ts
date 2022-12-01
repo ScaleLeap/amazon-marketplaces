3a0a84f2cb33da5932039420ff7ac221709e8b0e
Roman Filippov <rf@romanfilippov.com>
2022-12-01 23:09:16 +0000

    feat: adds advertising to Saudi Arabia

--- src/marketplaces/sa.ts
+++ src/marketplaces/sa.ts
@@ -1,9 +1,12 @@
 import {
   AmazonMarketplace,
+  AmazonMarketplaceAdvertisingCountryCode,
   AmazonMarketplaceAdvertisingCurrency,
+  AmazonMarketplaceAdvertisingTimeZone,
   AmazonMarketplaceCountryCode,
   AmazonMarketplaceTimeZone,
 } from '../amazon-marketplace'
+import { marketplaceAdvertisingRegions } from '../marketplace-advertising-regions'
 import { sellingPartnerRegions } from '../selling-partner-api-regions'
 
 export const SA = new AmazonMarketplace({
@@ -15,6 +18,22 @@ export const SA = new AmazonMarketplace({
   uri: 'https://www.amazon.sa',
   sellerCentralUri: 'https://sellercentral.amazon.sa',
   webServiceUri: 'https://mws-eu.amazonservices.com',
+  advertising: {
+    // UNDEFINED by Amazon
+    bids: {
+      sponsoredBrands: {
+        min: 1,
+        max: 1,
+      },
+      sponsoredProducts: {
+        min: 1,
+        max: 1,
+      },
+    },
+    countryCode: AmazonMarketplaceAdvertisingCountryCode.SA,
+    timeZone: AmazonMarketplaceAdvertisingTimeZone.ASIA_RIYADH,
+    region: marketplaceAdvertisingRegions.FE,
+  },
   sellingPartner: {
     region: sellingPartnerRegions.EU,
   },
